1959315b50afff9066d5516d6c1aa7d932867fc0
W.Xaver <w.xaver@googlemail.com>
2019-09-18 20:27:53 +0200

    fx bug with glyphcache management

--- src/main/java/de/mtplayer/mtp/controller/data/film/FilmlistFactory.java
+++ src/main/java/de/mtplayer/mtp/controller/data/film/FilmlistFactory.java
@@ -19,8 +19,15 @@ package de.mtplayer.mtp.controller.data.film;
 
 import de.mtplayer.mtp.controller.config.ProgData;
 import de.mtplayer.mtp.controller.history.HistoryList;
+import de.p2tools.p2Lib.tools.duration.PDuration;
+import de.p2tools.p2Lib.tools.log.PLog;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class FilmlistFactory {
+    private static Map<Character, Integer> counterMap = new HashMap<>(25);
+
     private FilmlistFactory() {
     }
 
@@ -42,25 +49,64 @@ public class FilmlistFactory {
 
     public static void cleanFaultyCharacterFilmlist() {
         // damit werden Unicode-Zeichen korrigiert
-        // gibt da eine Java-Bug
+        // gibt da einen Java-Bug
         // https://github.com/javafxports/openjdk-jfx/issues/287
+
+        PDuration.counterStart("cleanFaultyCharacter");
 
         Filmlist filmlist = ProgData.getInstance().filmlist;
         filmlist.stream().forEach(film -> {
-            film.arr[Film.FILM_TITLE] = cleanUnicode(film.getTitle());
-            film.arr[Film.FILM_THEME] = cleanUnicode(film.getTheme());
-            film.setDescription(cleanUnicode(film.getDescription()));
+            film.arr[Film.FILM_TITLE] = cleanUnicode_1(film.getTitle());
+            film.arr[Film.FILM_THEME] = cleanUnicode_1(film.getTheme());
+            film.setDescription(cleanUnicode_1(film.getDescription()));
+
+            film.arr[Film.FILM_TITLE] = cleanUnicode_2(film.getTitle());
+            film.arr[Film.FILM_THEME] = cleanUnicode_2(film.getTheme());
+            film.setDescription(cleanUnicode_2(film.getDescription()));
+
+            // U+3000 (12288)	　	Trenn- (Leer-) Zeichen	Whitespace	IDEOGRAPHIC SPACE	Ideographisches Leerzeichen
+            // das hat die Probleme gemacht, Film: Weltbilder
         });
+
+        for (Map.Entry<Character, Integer> entry : counterMap.entrySet()) {
+            Character key = entry.getKey();
+            Integer value = entry.getValue();
+            PLog.sysLog("Key: " + (int) key + "  Key: " + key + "  Anz: " + value);
+
+        }
+
+        PDuration.counterStop("cleanFaultyCharacter");
     }
+
 
     final static String regEx = "[\\p{Cc}&&[^\n,\r,\t]]";
 
-    public static String cleanUnicode(String ret) {
+    public static String cleanUnicode_1(String ret) {
         // damit werden Unicode-Zeichen korrigiert
         // gibt da eine Java-Bug
         // https://github.com/javafxports/openjdk-jfx/issues/287
 
         ret = ret.replaceAll(regEx, "");
         return ret;
+    }
+
+    public static String cleanUnicode_2(String test) {
+        // damit werden Unicode-Zeichen korrigiert
+        // gibt da eine Java-Bug, auch Probleme bei Linux mit fehlenden Zeichen in den code tablen
+        // https://github.com/javafxports/openjdk-jfx/issues/287
+
+        char[] c = test.toCharArray();
+
+        for (int i = 0; i < c.length; ++i) {
+
+            if ((int) c[i] > 11263) { // der Wert ist jetzt einfach mal geschätzt und kommt ~ 20x vor
+                counterMap.merge(c[i], 1, Integer::sum);
+                c[i] = ' ';
+                test = String.valueOf(c);
+            }
+
+        }
+
+        return test;
     }
 }
