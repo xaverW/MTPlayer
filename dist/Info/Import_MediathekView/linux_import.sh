cc2b746d72bbbd3e86c5caa2da34e4aafbf36503
W.Xaver <w.xaver@googlemail.com>
2019-09-26 14:55:30 +0200

    change project

--- dist/Info/Import_MediathekView/linux_import.sh
+++ dist/Info/Import_MediathekView/linux_import.sh
@@ -10,24 +10,34 @@ date=$(date +%y.%m.%d-%H:%M:%S)
 
 echo ========================
 echo Import Config
-echo Date:    $date
+echo Date:  $date
 echo ========================
 
 if [ -d "$mtdir" ];then
-	echo "rename mt config-dir"
-	echo $mtdir-$date
+	echo "rename old mt config-dir to:  " $mtdir-$date
 	mv $mtdir $mtdir-$date
 fi
 
-echo "create mt config-dir"
+echo "create new mt new config-dir: " $mtdir
 mkdir $mtdir
 
 if [ -d "$mvdir" ];then
-	echo "copy config"
+	
+	echo "copy abos"
 	cp $mvdir/downloadAbos.txt $mtdir/downloads.txt
 
+	
+	echo "copy history"
 	cp $mvdir/history.txt $mtdir/
-	cp $mvdir/mediathek.xml $mtdir/mtplayer.xml
+	
+
+	echo "copy config and change abo duration"
+	#cp $mvdir/mediathek.xml $mtdir/mtplayer.xml
+
+	sed -E 's/<Mindestdauer>([0-9]*)<\/Mindestdauer><min_max>true<\/min_max>/<Mindestdauer>\1<\/Mindestdauer><Maxdauer>150<\/Maxdauer>/g;
+	s/<Mindestdauer>([0-9]*)<\/Mindestdauer><min_max>false<\/min_max>/<Mindestdauer>0<\/Mindestdauer><Maxdauer>\1<\/Maxdauer>/g;
+	s/<Maxdauer>0<\/Maxdauer>/<Maxdauer>150<\/Maxdauer>/g' $mvdir/mediathek.xml > $mtdir/mtplayer.xml 
+	
 fi
 
 
