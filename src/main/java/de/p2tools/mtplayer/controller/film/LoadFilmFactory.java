ec16d53078a0688c910032c162f8c176bc2c8fb0
W.Xaver <w.xaver@googlemail.com>
2023-02-11 16:27:21 +0100

    loadFilmlist progStart

--- src/main/java/de/p2tools/mtplayer/controller/film/LoadFilmFactory.java
+++ src/main/java/de/p2tools/mtplayer/controller/film/LoadFilmFactory.java
@@ -129,6 +129,7 @@ public class LoadFilmFactory {
         LoadFactoryConst.debug = ProgData.debug;
         LoadFactoryConst.SYSTEM_LOAD_NOT_SENDER = ProgConfig.SYSTEM_LOAD_NOT_SENDER.getValue();
         LoadFactoryConst.DOWNLOAD_MAX_BANDWIDTH_KBYTE = ProgConfig.DOWNLOAD_MAX_BANDWIDTH_KBYTE;
+        LoadFactoryConst.downloadMaxBandwidth = ProgConfig.DOWNLOAD_MAX_BANDWIDTH_KBYTE.getValue();
         LoadFactoryConst.SYSTEM_LOAD_FILMLIST_MAX_DAYS = ProgConfig.SYSTEM_LOAD_FILMLIST_MAX_DAYS.getValue();
         LoadFactoryConst.SYSTEM_LOAD_FILMLIST_MIN_DURATION = ProgConfig.SYSTEM_LOAD_FILMLIST_MIN_DURATION.getValue();
         LoadFactoryConst.userAgent = ProgConfig.SYSTEM_USERAGENT.getValue();
