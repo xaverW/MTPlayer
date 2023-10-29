e30bd685b9d11209325b31f55b28b22d0b91cdd2
W.Xaver <w.xaver@googlemail.com>
2023-10-29 11:02:47 +0100

    download error mesg

--- src/main/java/de/p2tools/mtplayer/controller/starter/DownloadExternal.java
+++ src/main/java/de/p2tools/mtplayer/controller/starter/DownloadExternal.java
@@ -183,7 +183,7 @@ public class DownloadExternal extends Thread {
             retStatus = stat_start;
         } else {
             // dann wars das
-            errMsg.setValue("Programm zum Download konnte nicht gestartet werden.");
+            errMsg.setValue("Programm: zum Download [" + download.getProgramName() + "] konnte nicht gestartet werden.");
             retStatus = stat_finished_error;
         }
         return retStatus;
