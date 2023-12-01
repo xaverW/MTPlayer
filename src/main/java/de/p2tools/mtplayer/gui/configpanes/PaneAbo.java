600abe1c8e653136c053c1f0808a4e219969b926
W.Xaver <w.xaver@googlemail.com>
2023-12-01 13:50:37 +0100

    config dialog: abo info

--- src/main/java/de/p2tools/mtplayer/gui/configpanes/PaneAbo.java
+++ src/main/java/de/p2tools/mtplayer/gui/configpanes/PaneAbo.java
@@ -18,10 +18,13 @@ package de.p2tools.mtplayer.gui.configpanes;
 
 import de.p2tools.mtplayer.controller.config.ProgConfig;
 import de.p2tools.mtplayer.controller.config.ProgConfigAskBeforeDelete;
+import de.p2tools.mtplayer.controller.config.ProgData;
+import de.p2tools.mtplayer.controller.data.abo.AboData;
 import de.p2tools.mtplayer.gui.tools.HelpText;
 import de.p2tools.p2lib.P2LibConst;
 import de.p2tools.p2lib.guitools.P2Button;
 import de.p2tools.p2lib.guitools.P2ColumnConstraints;
+import de.p2tools.p2lib.guitools.P2GuiTools;
 import de.p2tools.p2lib.guitools.ptoggleswitch.P2ToggleSwitch;
 import javafx.geometry.Insets;
 import javafx.geometry.VPos;
@@ -41,6 +44,7 @@ public class PaneAbo {
     private final RadioButton rbOnlyStopAsk = new RadioButton("Vor dem Löschen fragen");
     private final RadioButton rbOnlyStopDelete = new RadioButton("Abo sofort löschen ohne zu fragen");
     private final Stage stage;
+    private final VBox vBoxAll = new VBox();
 
     public PaneAbo(Stage stage) {
         this.stage = stage;
@@ -53,13 +57,19 @@ public class PaneAbo {
     }
 
     public void makeAbo(Collection<TitledPane> result) {
-        final GridPane gridPane = new GridPane();
-        TitledPane tpConfig = new TitledPane("Abo", gridPane);
+        TitledPane tpConfig = new TitledPane("Abo", vBoxAll);
         result.add(tpConfig);
+        vBoxAll.setSpacing(P2LibConst.DIST_VBOX);
+        addGridPane();
+        addAboInfo();
+    }
 
+    private void addGridPane() {
+        final GridPane gridPane = new GridPane();
         gridPane.setHgap(P2LibConst.DIST_GRIDPANE_HGAP);
         gridPane.setVgap(P2LibConst.DIST_GRIDPANE_VGAP);
         gridPane.setPadding(new Insets(P2LibConst.DIST_EDGE));
+        vBoxAll.getChildren().add(gridPane);
 
         tglSearchAbo.selectedProperty().bindBidirectional(ProgConfig.ABO_SEARCH_NOW);
         final Button btnHelpAbo = P2Button.helpButton(stage, "Abos automatisch suchen",
@@ -122,5 +132,48 @@ public class PaneAbo {
                 rbOnlyStopAsk.setSelected(true);
                 break;
         }
+    }
+
+    private void addAboInfo() {
+        int aboActive = 0, aboInactive = 0, aboActiveHits = 0, aboCountNoHits = 0;
+        for (AboData aboData : ProgData.getInstance().aboList) {
+            if (aboData.isActive()) {
+                ++aboActive;
+                aboActiveHits += aboData.getHit();
+                if (aboData.getHit() == 0) {
+                    ++aboCountNoHits;
+                }
+
+            } else {
+                ++aboInactive;
+            }
+        }
+
+        final GridPane gridPane = new GridPane();
+        gridPane.setHgap(P2LibConst.DIST_GRIDPANE_HGAP);
+        gridPane.setVgap(P2LibConst.DIST_GRIDPANE_VGAP);
+        gridPane.setPadding(new Insets(P2LibConst.DIST_EDGE));
+        gridPane.getStyleClass().add("extra-pane");
+        gridPane.getColumnConstraints().addAll(P2ColumnConstraints.getCcPrefSize(),
+                P2ColumnConstraints.getCcPrefSizeRight(),
+                P2ColumnConstraints.getCcPrefSize(),
+                P2ColumnConstraints.getCcPrefSize(),
+                P2ColumnConstraints.getCcComputedSizeAndHgrow());
+        vBoxAll.getChildren().addAll(P2GuiTools.getVBoxGrower(), gridPane);
+
+        int row = 0;
+        gridPane.add(new Label("Aktive Abos:"), 0, row);
+        gridPane.add(new Label(aboActive + ""), 1, row);
+        gridPane.add(new Label(" "), 2, row);
+        gridPane.add(new Label("( Treffer:"), 3, row);
+        gridPane.add(new Label(aboActiveHits + " )"), 4, row);
+
+        ++row;
+        gridPane.add(new Label("Aktive Abos ohne Treffer:"), 0, ++row);
+        gridPane.add(new Label(aboCountNoHits + ""), 1, row);
+
+        ++row;
+        gridPane.add(new Label("Inaktive Abos:"), 0, ++row);
+        gridPane.add(new Label(aboInactive + ""), 1, row);
     }
 }
