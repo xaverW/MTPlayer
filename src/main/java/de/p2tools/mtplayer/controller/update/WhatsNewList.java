335d5d79280cda38aec821fd40a200532bf1414b
W.Xaver <w.xaver@googlemail.com>
2024-05-23 14:27:00 +0200

    whatsNew

--- src/main/java/de/p2tools/mtplayer/controller/update/WhatsNewList.java
+++ src/main/java/de/p2tools/mtplayer/controller/update/WhatsNewList.java
@@ -39,7 +39,17 @@ public class WhatsNewList extends ArrayList<WhatsNewInfo> {
     }
 
     private void addWhatsNew() {
-        WhatsNewInfo whatsNewInfo = new WhatsNewInfo(LocalDate.of(2024, 2, 11),
+        WhatsNewInfo whatsNewInfo = new WhatsNewInfo(LocalDate.of(2024, 4, 1),
+                "/de/p2tools/mtplayer/res/whatsnew/whatsNew_08.png",
+
+                "Live-Suche",
+                "MTPlayer hat jetzt auch eine Live-Suche in den Mediatheken von ARD und ZDF. " +
+                        "Damit können Filme mit einem Suchbegriff live gesucht werden. Es ist auch möglich " +
+                        "einen Film mit der URL einer Filmseite aus der Mediathek anzulegen. Die Live-Suche kann " +
+                        "mit dem Menübutton (oder über das Programmmenü) ein- und ausgeblendet werden.", 110);
+        add(whatsNewInfo);
+
+        whatsNewInfo = new WhatsNewInfo(LocalDate.of(2024, 2, 11),
                 "/de/p2tools/mtplayer/res/whatsnew/whatsNew_01.png",
 
                 "Doppelte Filme",
@@ -53,23 +63,13 @@ public class WhatsNewList extends ArrayList<WhatsNewInfo> {
                 "/de/p2tools/mtplayer/res/whatsnew/whatsNew_02.png",
 
                 "Filme mit Untertitel",
-                "Es gibt Filme, die den Untertitel im \"Film\" anzeigen. " +
+                "Es gibt Filme, die den Untertitel im \"Filmtitel\" anzeigen. " +
                         "In den Einstellungen kann angegeben werden " +
                         "ob diese Filme auch als \"Film mit Untertitel\" geführt werden sollen. " +
                         "Für welche Filme das dann zutrifft, kann man hier vorgegeben." +
                         "\n\n" +
                         "Im Kontextmenü in der Tabelle mit den Filmen, gibt es jetzt einen weiteren Menüpunkt. " +
                         "Damit können die Untertitel-Dateien direkt heruntergeladen werden.", 150);
-        add(whatsNewInfo);
-
-        whatsNewInfo = new WhatsNewInfo(LocalDate.of(2024, 2, 11),
-                "/de/p2tools/mtplayer/res/whatsnew/whatsNew_03.png",
-
-                "Filmfilter \"Thema exact\"",
-                "Es ist jetzt möglich, durch Texteingabe die angezeigte Liste der Themen " +
-                        "zu filtern\n" +
-                        "Mit ENTER wird das selektierte Theme gewählt. Um ein Thema auszuwählen, können die " +
-                        "Courser-Tasten und die Tab-Tasten benutzt werden.", 100);
         add(whatsNewInfo);
 
         whatsNewInfo = new WhatsNewInfo(LocalDate.of(2024, 2, 11), "/de/p2tools/mtplayer/res/whatsnew/whatsNew_04.png",
@@ -95,25 +95,6 @@ public class WhatsNewList extends ArrayList<WhatsNewInfo> {
                         "kann ein- und ausgeschaltet werden. Das ist im Kontextmenü der Tabelle möglich.", 70);
         add(whatsNewInfo);
 
-        whatsNewInfo = new WhatsNewInfo(LocalDate.of(2024, 3, 3),
-                "/de/p2tools/mtplayer/res/whatsnew/whatsNew_07.png",
-
-                "Filmfilter",
-                "Die verwendeten Filmfilter werden jetzt gespeichert und sind bei " +
-                        "einem Programmneustart wieder vorhanden. " +
-                        "Eine Auswahl listet sie auf und können so auch ausgewählt werden.", 70);
-        add(whatsNewInfo);
-
-        whatsNewInfo = new WhatsNewInfo(LocalDate.of(2024, 4, 1),
-                "/de/p2tools/mtplayer/res/whatsnew/whatsNew_08.png",
-
-                "Live-Suche",
-                "MTPlayer hat jetzt auch eine Live-Suche in den Mediatheken von ARD und ZDF. " +
-                        "Damit können Filme mit einem Suchbegriff live gesucht werden. Es ist auch möglich " +
-                        "einen Film mit der URL einer Filmseite aus der Mediathek anzulegen. Die Live-Suche kann " +
-                        "mit dem Menübutton (oder über das Programmmenü) ein- und ausgeblendet werden.", 110);
-        add(whatsNewInfo);
-
         whatsNewInfo = new WhatsNewInfo(LocalDate.of(2024, 4, 6),
                 "/de/p2tools/mtplayer/res/whatsnew/whatsNew_09.png",
 
@@ -133,16 +114,38 @@ public class WhatsNewList extends ArrayList<WhatsNewInfo> {
                         "in der rechten Menüleiste blendet sie auch aus.", 70);
         add(whatsNewInfo);
 
+        whatsNewInfo = new WhatsNewInfo(LocalDate.of(2024, 2, 11),
+                "/de/p2tools/mtplayer/res/whatsnew/whatsNew_03.png",
+
+                "Filmfilter \"Thema exact\"",
+                "Es ist jetzt möglich, durch Texteingabe die angezeigte Liste der Themen " +
+                        "zu filtern\n" +
+                        "Mit ENTER wird das selektierte Thema gewählt. Um ein Thema auszuwählen, können die " +
+                        "Courser-Tasten und die Tab-Tasten benutzt werden.", 100);
+        add(whatsNewInfo);
+
+        whatsNewInfo = new WhatsNewInfo(LocalDate.of(2024, 3, 3),
+                "/de/p2tools/mtplayer/res/whatsnew/whatsNew_07.png",
+
+                "Filmfilter",
+                "Die verwendeten Textfilter im Filmfilter werden " +
+                        "in einer eigenen Auswahl gespeichert und sind bei " +
+                        "einem Programmneustart wieder vorhanden. " +
+                        "Eine Auswahl listet sie auf und sie können so auch ausgewählt werden. Es werden " +
+                        "dann die Textfilter gefüllt, die anderen Filter bleiben dabei unverändert.", 100);
+        add(whatsNewInfo);
+
         whatsNewInfo = new WhatsNewInfo(LocalDate.of(2024, 5, 24),
                 "/de/p2tools/mtplayer/res/whatsnew/whatsNew_11.png",
 
                 "Textfilter",
-                "Die Textfilter werden jetzt gespeichert. Im Filter kann mit dem Button vor dem Eintrag " +
+                "Alle Textfilter im Programm werden jetzt gespeichert. " +
+                        "Im Filter kann mit dem Button vor dem Eintrag " +
                         "dieser gelöscht werden. Der erste Eintrag löscht den Filter und der " +
                         "Button davor löscht alle Einträge in dem Filter.", 70);
         add(whatsNewInfo);
 
-        whatsNewInfo = new WhatsNewInfo(LocalDate.of(2024, 3, 3), "",
+        whatsNewInfo = new WhatsNewInfo(LocalDate.of(2024, 4, 6), "",
                 "Was sich sonst noch geändert hat",
                 "* Die Filme des Senders \"Radio Bremen TV\" wurden mit dem Sendernamen \"Radio Bremen TV\", " +
                         "\"rbtv\" und \"RBTV\" gelistet. Die werden jetzt zusammengefasst zu dem " +
@@ -151,17 +154,13 @@ public class WhatsNewList extends ArrayList<WhatsNewInfo> {
                         "* Es gibt ein neues ShortCut zum Anzeigen der Blacklist-Einstellungen: ALT+B" +
                         "\n\n" +
                         "* In der Ersetzungstabelle für Download-Namen (Einstellungen->Download) " +
-                        "können jetzt auch RegEx verwendet werden.",
-                180);
-        add(whatsNewInfo);
-
-        whatsNewInfo = new WhatsNewInfo(LocalDate.of(2024, 4, 6), "",
-                "Was sich sonst noch geändert hat",
-                "* Bei der Startzeit von Downloads kann jetzt die Zeit von 00:00 bis 23:45 Uhr " +
+                        "können jetzt auch RegEx verwendet werden." +
+                        "\n\n" +
+                        "* Bei der Startzeit von Downloads kann jetzt die Zeit von 00:00 bis 23:45 Uhr " +
                         "angegeben werden. Liegt die Zeit in der Vergangenheit wird der Download morgen um " +
                         "diese Zeit gestartet. Z.B. es ist 20:00 Uhr und der Download wird mit Startzeit: 05:00 Uhr " +
                         "gestartet, dann startet der Download morgen um 05:00 Uhr.",
-                100);
+                300);
         add(whatsNewInfo);
     }
 }
