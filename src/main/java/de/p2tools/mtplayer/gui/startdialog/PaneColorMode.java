81d1ff97f6ffc4465006e8f3aad48d01e32a629b
W.Xaver <w.xaver@googlemail.com>
2024-12-27 17:50:24 +0100

    startdialog color

--- src/main/java/de/p2tools/mtplayer/gui/startdialog/PaneColorMode.java
+++ src/main/java/de/p2tools/mtplayer/gui/startdialog/PaneColorMode.java
@@ -22,6 +22,7 @@ import de.p2tools.p2lib.P2LibConst;
 import de.p2tools.p2lib.guitools.P2Button;
 import de.p2tools.p2lib.guitools.P2ColumnConstraints;
 import de.p2tools.p2lib.guitools.ptoggleswitch.P2ToggleSwitch;
+import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -29,6 +30,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TitledPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -39,6 +41,10 @@ public class PaneColorMode {
     HBox hBoxColor = new HBox();
     private final P2ToggleSwitch tglDarkTheme = new P2ToggleSwitch("Dunkles Erscheinungsbild der Programmoberfläche");
     private final P2ToggleSwitch tglBlackWhiteIcon = new P2ToggleSwitch("Schwarz-Weiße Icons");
+    private final HBox hBoxImage0 = new HBox(); // !weiß / !dark
+    private final HBox hBoxImage1 = new HBox(); // weiß / !dark
+    private final HBox hBoxImage2 = new HBox(); // !weiß / dark
+    private final HBox hBoxImage3 = new HBox(); // weiß / dark
 
     public PaneColorMode(Stage stage) {
         this.stage = stage;
@@ -50,6 +56,8 @@ public class PaneColorMode {
     }
 
     public TitledPane make() {
+        makeImage();
+        setHBox();
         VBox vBox = new VBox(10);
         hBoxColor.setAlignment(Pos.CENTER);
 
@@ -71,7 +79,7 @@ public class PaneColorMode {
         final GridPane gridPane = new GridPane();
         gridPane.setHgap(P2LibConst.DIST_GRIDPANE_HGAP);
         gridPane.setVgap(P2LibConst.DIST_GRIDPANE_VGAP);
-        gridPane.setPadding(new Insets(P2LibConst.PADDING));
+//        gridPane.setPadding(new Insets(P2LibConst.PADDING));
 
         gridPane.add(tglDarkTheme, 0, row);
         gridPane.add(btnHelpTheme, 1, row);
@@ -90,10 +98,10 @@ public class PaneColorMode {
         gridPaneGui.setVgap(P2LibConst.DIST_GRIDPANE_VGAP);
         gridPaneGui.setPadding(new Insets(P2LibConst.PADDING));
 
-        gridPaneGui.add(getImage(0, true), 0, 0);
-        gridPaneGui.add(getImage(1, true), 1, 0);
-        gridPaneGui.add(getImage(2, true), 0, 1);
-        gridPaneGui.add(getImage(3, true), 1, 1);
+        gridPaneGui.add(hBoxImage0, 0, 0);
+        gridPaneGui.add(hBoxImage1, 1, 0);
+        gridPaneGui.add(hBoxImage2, 0, 1);
+        gridPaneGui.add(hBoxImage3, 1, 1);
 
         gridPaneGui.getColumnConstraints().addAll(P2ColumnConstraints.getCcPrefSize(),
                 P2ColumnConstraints.getCcPrefSize());
@@ -104,32 +112,112 @@ public class PaneColorMode {
     }
 
     private void setHBox() {
-        hBoxColor.getChildren().clear();
-
-        if (!tglDarkTheme.isSelected() && !tglBlackWhiteIcon.isSelected()) {
-            // hell / bunt
-            hBoxColor.getChildren().add(getImage(0, false));
-
-        } else if (!tglDarkTheme.isSelected() && tglBlackWhiteIcon.isSelected()) {
-            // dark / bunt
-            hBoxColor.getChildren().add(getImage(1, false));
-
-        } else if (tglDarkTheme.isSelected() && !tglBlackWhiteIcon.isSelected()) {
-            // hell / weiß
-            hBoxColor.getChildren().add(getImage(2, false));
-
+        int i;
+        if (!ProgConfig.SYSTEM_BLACK_WHITE_ICON_START.get() && !ProgConfig.SYSTEM_DARK_THEME_START.get()) {
+            i = 0;
+        } else if (ProgConfig.SYSTEM_BLACK_WHITE_ICON_START.get() && !ProgConfig.SYSTEM_DARK_THEME_START.get()) {
+            i = 1;
+        } else if (!ProgConfig.SYSTEM_BLACK_WHITE_ICON_START.get() && ProgConfig.SYSTEM_DARK_THEME_START.get()) {
+            i = 2;
         } else {
-            // dark / weiß
-            hBoxColor.getChildren().add(getImage(3, false));
+            i = 3;
+        }
+
+        final String colorSel = "#4682B4;";
+//        final String color = "#bdbdbd;";
+        final String color = "transparent;";
+
+        switch (i) {
+            case 0 -> {
+                hBoxImage0.setStyle("-fx-border-color: " + colorSel + " -fx-border-style: solid; -fx-border-width: 8;");
+                hBoxImage1.setStyle("-fx-border-color: " + color + " -fx-border-style: solid; -fx-border-width: 8;");
+                hBoxImage2.setStyle("-fx-border-color: " + color + " -fx-border-style: solid; -fx-border-width: 8;");
+                hBoxImage3.setStyle("-fx-border-color: " + color + " -fx-border-style: solid; -fx-border-width: 8;");
+            }
+            case 1 -> {
+                hBoxImage0.setStyle("-fx-border-color: " + color + " -fx-border-style: solid; -fx-border-width: 8;");
+                hBoxImage1.setStyle("-fx-border-color: " + colorSel + " -fx-border-style: solid; -fx-border-width: 8;");
+                hBoxImage2.setStyle("-fx-border-color: " + color + " -fx-border-style: solid; -fx-border-width: 8;");
+                hBoxImage3.setStyle("-fx-border-color: " + color + " -fx-border-style: solid; -fx-border-width: 8;");
+            }
+            case 2 -> {
+                hBoxImage0.setStyle("-fx-border-color: " + color + " -fx-border-style: solid; -fx-border-width: 8;");
+                hBoxImage1.setStyle("-fx-border-color: " + color + " -fx-border-style: solid; -fx-border-width: 8;");
+                hBoxImage2.setStyle("-fx-border-color: " + colorSel + " -fx-border-style: solid; -fx-border-width: 8;");
+                hBoxImage3.setStyle("-fx-border-color: " + color + " -fx-border-style: solid; -fx-border-width: 8;");
+            }
+            case 3 -> {
+                hBoxImage0.setStyle("-fx-border-color: " + color + " -fx-border-style: solid; -fx-border-width: 8;");
+                hBoxImage1.setStyle("-fx-border-color: " + color + " -fx-border-style: solid; -fx-border-width: 8;");
+                hBoxImage2.setStyle("-fx-border-color: " + color + " -fx-border-style: solid; -fx-border-width: 8;");
+                hBoxImage3.setStyle("-fx-border-color: " + colorSel + " -fx-border-style: solid; -fx-border-width: 8;");
+            }
         }
     }
 
-    private ImageView getImage(int i, boolean vorschau) {
-        final String path = "/de/p2tools/mtplayer/res/startdialog/gui_color_" + i + (vorschau ? "_" : "") + ".png";
-        Image image = new Image(path, vorschau ? 400 : 600, vorschau ? 400 : 600, true, true);
-        ImageView iv = new ImageView();
-        iv.setSmooth(true);
-        iv.setImage(image);
-        return iv;
+    private void makeImage() {
+        ImageView iv0 = new ImageView();
+        hBoxImage0.getChildren().add(iv0);
+        String path = "/de/p2tools/mtplayer/res/startdialog/gui_color_" + 0 + ".png";
+        Image image = new Image(path, 400, 400, true, true);
+        iv0.setSmooth(true);
+        iv0.setImage(image);
+        iv0.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                tglBlackWhiteIcon.setSelected(false);
+                tglDarkTheme.setSelected(false);
+                setHBox();
+                event.consume();
+            }
+        });
+
+        ImageView iv1 = new ImageView();
+        hBoxImage1.getChildren().add(iv1);
+        path = "/de/p2tools/mtplayer/res/startdialog/gui_color_" + 1 + ".png";
+        image = new Image(path, 400, 400, true, true);
+        iv1.setSmooth(true);
+        iv1.setImage(image);
+        iv1.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                tglBlackWhiteIcon.setSelected(true);
+                tglDarkTheme.setSelected(false);
+                setHBox();
+                event.consume();
+            }
+        });
+
+        ImageView iv2 = new ImageView();
+        hBoxImage2.getChildren().add(iv2);
+        path = "/de/p2tools/mtplayer/res/startdialog/gui_color_" + 2 + ".png";
+        image = new Image(path, 400, 400, true, true);
+        iv2.setSmooth(true);
+        iv2.setImage(image);
+        iv2.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                tglBlackWhiteIcon.setSelected(false);
+                tglDarkTheme.setSelected(true);
+                setHBox();
+                event.consume();
+            }
+        });
+
+        ImageView iv3 = new ImageView();
+        hBoxImage3.getChildren().add(iv3);
+        path = "/de/p2tools/mtplayer/res/startdialog/gui_color_" + 3 + ".png";
+        image = new Image(path, 400, 400, true, true);
+        iv3.setSmooth(true);
+        iv3.setImage(image);
+        iv3.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                tglBlackWhiteIcon.setSelected(true);
+                tglDarkTheme.setSelected(true);
+                setHBox();
+                event.consume();
+            }
+        });
     }
 }
