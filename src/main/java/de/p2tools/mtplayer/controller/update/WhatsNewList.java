5cdee9b32f9d12d5f92e882185b21748a1790778
W.Xaver <w.xaver@googlemail.com>
2025-12-28 08:24:12 +0100

    whatsNew dialog

--- src/main/java/de/p2tools/mtplayer/controller/update/WhatsNewList.java
+++ src/main/java/de/p2tools/mtplayer/controller/update/WhatsNewList.java
@@ -42,7 +42,7 @@ public class WhatsNewList extends ArrayList<WhatsNewInfo> {
                 "Die Bookmarks können in einem eigenen Dialog " +
                         "angezeigt werden. Dort können die Filme auch " +
                         "angesehen und gespeichert werden. Es können auch Kommentare " +
-                        "zu den Bookmarks hinzugefügt werden.", 100);
+                        "zu den Bookmarks hinzugefügt werden.", 80);
         add(whatsNewInfo);
 
         whatsNewInfo = new WhatsNewInfo(LocalDate.of(2025, 7, 29),
@@ -52,7 +52,7 @@ public class WhatsNewList extends ArrayList<WhatsNewInfo> {
                 "Im Tab Filme kann ein einfacher Filter " +
                         "angezeigt werden. Er enthält nur das nötigste und ist " +
                         "übersichtlicher. Umschalten lässt sich das " +
-                        "im Film-Menü->Einfacher Filter.", 100);
+                        "im Film-Menü->Einfacher Filter.", 80);
         add(whatsNewInfo);
 
         whatsNewInfo = new WhatsNewInfo(LocalDate.of(2025, 8, 25),
@@ -64,17 +64,30 @@ public class WhatsNewList extends ArrayList<WhatsNewInfo> {
                         "abgespielt und aufgezeichnet werden. Auch Abos lassen sich " +
                         "anlegen.\n" +
                         "Wer die Audiothek nicht verwenden will, kann sie in den Einstellungen " +
-                        "abschalten.\n" +
-                        "Wenns Vorschläge dazu gibt, gerne per Mail oder im Forum.", 140);
+                        "abschalten.", 100);
         add(whatsNewInfo);
 
         whatsNewInfo = new WhatsNewInfo(LocalDate.of(2025, 10, 11),
-                "",
+                "/de/p2tools/mtplayer/res/whatsnew/whatsNew_filter-vorschlag.png",
                 "Filtervorschläge",
                 "In den Textfiltern (Filme/Audios) kann mit einem Doppelklick " +
                         "ein Filtervorschlag eingefügt werden. Die Vorschläge kann man in " +
                         "den \"Einstellungen->Filter\" vorgeben. Die Funktion kann dort auch " +
-                        "abgeschaltet werden.", 100);
+                        "abgeschaltet werden.", 80);
         add(whatsNewInfo);
+
+
+        whatsNewInfo = new WhatsNewInfo(LocalDate.of(2025, 12, 27),
+                "/de/p2tools/mtplayer/res/whatsnew/whatsNew_icon_set.png",
+
+                "Iconset",
+                "Das Programm hat ein neues Iconset, die Icons sind jetzt Vektorgrafiken. " +
+                        "Der Vorteil ist, dass diese verlustfrei skaliert werden können. " +
+                        "Weiter können sie beliebig eingefärbt werden. Es wurde auch das komplette " +
+                        "Erscheinungsbild der Programmoberfläche aktualisiert und die " +
+                        "Programmoberfläche lässt sich nach dem eigenem Gusto, farblich gestalten.", 120);
+        add(whatsNewInfo);
+
+
     }
 }
