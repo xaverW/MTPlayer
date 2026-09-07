3fdaa3ddc6ec61c3e94d0da6ea3ef5033d01227f
W.Xaver <w.xaver@googlemail.com>
2026-09-07 17:43:58 +0200

    icon

--- src/main/java/de/p2tools/mtplayer/gui/dialog/ResetDialogController.java
+++ src/main/java/de/p2tools/mtplayer/gui/dialog/ResetDialogController.java
@@ -114,7 +114,7 @@ public class ResetDialogController extends P2DialogExtra {
             }
         });
 
-        gridPane.add(P2IconFactory.P2ICON.P2_ATTENTION_80.getFontIcon(), 0, 0, 1, 1);
+        gridPane.add(P2IconFactory.P2ICON.P2_ATTENTION_OCT_80.getFontIcon(), 0, 0, 1, 1);
         gridPane.add(headerLabel, 1, 0);
         gridPane.add(cancelButton, 1, 1);
         gridPane.add(btnHelp, 2, 1);
